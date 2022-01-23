a3cae12825eaf03e49f2973b3760e2740d8fd0bc
André Lademann <vergissberlin@googlemail.com>
2022-01-23 23:16:40 +0100

    Add package json

--- data/httpd/main.js
+++
@@ -1,4 +0,0 @@
-var button = document.getElementById('button');
-button.addEventListener('click', function () {
-    console.log('%c 🌈  Oh my heavens! You have finally clicked the button!', 'background: pink url(img/boom.webp) no-repeat; color: #333;padding: 1rem; height: 200px');
-});
